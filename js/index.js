85173f649d432cfb3492ca0e55ae70799bb6d390
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-03-09 15:30:01 -0800

    examples 2-4

--- js/index.js
+++ js/index.js
@@ -1,9 +1,38 @@
 // Your code goes here
+// Type 1 
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(btn => {btn.addEventListener('click',
+ (event) => { event.target.style.backgroundColor = 'blue'; });
+})
 
-let buttons = document.getElementsByClassName('.btn');
+// Type 2 
+const links = document.querySelectorAll('.nav');
+links.forEach(link => {link.addEventListener('mouseover',
+ (event) => { event.target.style.color = 'red'; });
+})
 
-buttons.addEventListener("click", myfunc); 
+// type 3
+const p = document.querySelectorAll('p');
+Array.from(p).forEach(Element => {
+    Element.addEventListener('dblclick', (event => {
+        event.target.style.backgroundColor = 'pink';
+    }));
+})
 
-function myfunc(){
-    alert('Hello World');
-}
+//type 4 
+
+const h4 = document.querySelectorAll('h4');
+Array.from(h4).forEach(Element => {
+    Element.addEventListener('dblclick', (event => {
+        event.target.style.backgroundColor = 'pink';
+    }));
+})
+
+// type 5
+
+const funBus = document.querySelectorAll('h1');
+Array.from(funBus).forEach(Element => {
+    Element.addEventListener('dblclick', (event => {
+        event.target.style.border-box = '1px solid black';
+    }));
+})
